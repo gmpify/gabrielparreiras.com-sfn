303f07a4fece1ccc932f9f830a6eeaba7b9171f4
Gabriel Parreiras <gab.parreiras@gmail.com>
2018-05-14 09:53:25 +0100

    feature: Make s3 bucket publicy available

--- sparkleformation/blog.rb
+++ sparkleformation/blog.rb
@@ -8,6 +8,28 @@ SparkleFormation.new(:blog, :provider => :aws) do
   dynamic!(:s3_bucket, :blog) do
     properties do
       bucket_name ref!(:bucket_name)
+      website_configuration do
+        error_document '404.html'
+        index_document 'index.html'
+      end
+    end
+  end
+
+  dynamic!(:s3_bucket_policy, :blog) do
+    properties do
+      bucket ref!(:blog_s3_bucket)
+      policy_document do
+        version '2012-10-17'
+        statement _array(
+          -> {
+            sid 'AllowPublicRead'
+            effect 'Allow'
+            principal '*'
+            action 's3:GetObject'
+            resource join!(['arn:aws:s3:::', ref!(:bucket_name), '/*'])
+          }
+        )
+      end
     end
   end
 end
